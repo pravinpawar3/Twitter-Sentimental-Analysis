0258618c2258cdce8f358ba60f734a4b0ce74cdd
Pravin Pawar <pravinpawar381997@gmail.com>
2023-08-24 02:45:13 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 import urllib.parse
-MONGO_CONNECTION_URL = "cluster0.cyoe45m.mongodb.net"
+MONGO_CONNECTION_URL = "clustername"
 DATABASE_NAME = "test"
-USERNAME = "prav3"
-PASSWORD = "Zfo5Q7wu16FsRkn7"
+USERNAME = "username"
+PASSWORD = "password"
 MONGO_CONNECTION_URL = f"mongodb+srv://{urllib.parse.quote_plus(USERNAME)}:{urllib.parse.quote_plus(PASSWORD)}@{MONGO_CONNECTION_URL}/{DATABASE_NAME}?retryWrites=true&w=majority"
